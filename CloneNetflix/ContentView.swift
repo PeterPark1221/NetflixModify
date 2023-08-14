c2f29f737ebb55b86df51967f816a2e2adfbb71f
HoYeon <137192346+PeterPark1221@users.noreply.github.com>
2023-08-14 22:00:57 +0900

    add tabview

--- CloneNetflix/ContentView.swift
+++ CloneNetflix/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().isTranslucent = true
+        UITabBar.appearance().barTintColor = UIColor.black 
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }.tag(0)
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Seach")
+                }.tag(1)
+            
+            Text("Coming soon")
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Coming soon")
+                }.tag(2)
+            
+            Text("Downloads")
+                .tabItem {
+                    Image(systemName: "arrow.down.to.line.alt")
+                    Text("Download")
+                }.tag(3)
+            
+            Text("More")
+                .tabItem {
+                    Image(systemName: "line.3.horizontal")
+                    Text("More")
+                }.tag(4)
         }
-        .padding()
+        .accentColor(.white)
     }
 }
 
